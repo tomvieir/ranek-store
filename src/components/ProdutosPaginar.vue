576194645b949c7205da78921c17780ae617a64c
tomvieir <wevertonbvieira@gmail.com>
2022-07-14 15:59:38 -0300

    pagination and search engine

---
+++ src/components/ProdutosPaginar.vue
@@ -0,0 +1,25 @@
+<template>
+<h1>paginação {{produtosPorPagina}} {{produtosTotal}}</h1>
+  
+</template>
+
+<script>
+export default {
+    name: "ProdutosPaginar",
+    props: {
+        produtosPorPagina: {
+            type: Number,
+            default: 1
+        },
+        produtosTotal: {
+            type: Number,
+            default: 1
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
